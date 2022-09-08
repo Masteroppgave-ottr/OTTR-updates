7c276d20cb3c40d93aae4a4c06bc2f6be988c577
Eckhoff42 <magnus.wiik.eckhoff@gmail.com>
2022-09-08 15:07:43 +0200

    lese fil

--- ottr-update/src/main/java/update/ottr/App.java
+++ ottr-update/src/main/java/update/ottr/App.java
@@ -1,10 +1,26 @@
 package update.ottr;
 
+import java.util.stream.Stream;
+
+import org.antlr.v4.runtime.CharStream;
+
+import xyz.ottr.lutra.stottr.io.SFileReader;
 import xyz.ottr.lutra.system.Result;
+import xyz.ottr.lutra.system.ResultStream;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SFileReader reader = new SFileReader();
+        ResultStream<CharStream> res = reader.apply("/home/magnus/Emner/Master/dev/temp/templates.stottr");
+
+        //print content in res
+        Stream<Result<CharStream>> stream = res.getStream();
+        
+        stream.forEach(r -> {
+            System.out.println(r.get().toString());
+        });
+
     }
 }
