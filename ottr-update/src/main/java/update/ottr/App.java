04c9e152cb354b59a8625ad6c63b1b528c5b8dd8
Preben <preben.zahl@gmail.com>
2022-09-20 11:08:11 +0200

    tester

--- ottr-update/src/main/java/update/ottr/App.java
+++ ottr-update/src/main/java/update/ottr/App.java
@@ -1,38 +1,26 @@
 package update.ottr;
 
-import java.util.stream.Stream;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import org.antlr.v4.runtime.CharStream;
-import org.apache.jena.sparql.function.library.leviathan.log;
-
-import xyz.ottr.lutra.system.Message;
-import xyz.ottr.lutra.model.Signature;
-import xyz.ottr.lutra.stottr.io.SFileReader;
-import xyz.ottr.lutra.stottr.parser.SInstanceParser;
-import xyz.ottr.lutra.stottr.parser.STemplateParser;
-import xyz.ottr.lutra.system.Assertions;
-import xyz.ottr.lutra.system.ResultConsumer;
+import xyz.ottr.lutra.api.StandardFormat;
+import xyz.ottr.lutra.api.StandardTemplateManager;
+import xyz.ottr.lutra.io.Format;
+import xyz.ottr.lutra.io.Format.Support;
+import xyz.ottr.lutra.store.TemplateStore;
+import xyz.ottr.lutra.stottr.StottrFormat;
 import xyz.ottr.lutra.system.ResultStream;
 
 public class App 
 {
     public static void main( String[] args )
+    //run this in ottr-update folder:
+    //java -cp target/ottr-update-1.0-SNAPSHOT.jar:../lutra.jar update.ottr.App
     {
-        SFileReader reader = new SFileReader();
-        
-        /* parse templates */
-        ResultStream<CharStream> templateStream = reader.apply("/home/magnus/Emner/Master/dev/temp/templates.stottr");
-        // templateStream.forEach(r -> {
-        //     System.out.println(r.get().toString() + "\n");
-        // });
-
-        String templateStr = templateStream.getStream().findFirst().get().get().toString();
-        STemplateParser sParser = new STemplateParser();
-        ResultConsumer<Signature> consumer = new ResultConsumer<>();            
-        sParser.parseString(templateStr).forEach(result -> System.out.println("Parsed :" + result));
-        
-        
-        /* parse instances */
-        ResultStream<CharStream> instanceStream = reader.apply("/home/magnus/Emner/Master/dev/temp/instances.stottr");        
+        StandardTemplateManager stm = new StandardTemplateManager();
+        // TemplateStore tStore = stm.getStandardLibrary();
+        Format format = new StottrFormat();
+        ResultStream resultStream = stm.readInstances(format, "home/ottr/ottr-update/src/main/resources/instances");
+        resultStream.flatMap(Function.identity());
     }
 }
