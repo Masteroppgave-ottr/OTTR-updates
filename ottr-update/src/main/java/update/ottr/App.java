abdc5e44cee475e3e4a9c8fb1590beb36e67adc2
Leif Harald Karlsen <leifhka@ifi.uio.no>
2022-09-20 11:29:01 +0200

    Add example to main.

--- ottr-update/src/main/java/update/ottr/App.java
+++ ottr-update/src/main/java/update/ottr/App.java
@@ -5,10 +5,13 @@ import java.util.stream.Collectors;
 
 import xyz.ottr.lutra.api.StandardFormat;
 import xyz.ottr.lutra.api.StandardTemplateManager;
+import xyz.ottr.lutra.TemplateManager;
+import xyz.ottr.lutra.model.Instance;
 import xyz.ottr.lutra.io.Format;
 import xyz.ottr.lutra.io.Format.Support;
 import xyz.ottr.lutra.store.TemplateStore;
 import xyz.ottr.lutra.stottr.StottrFormat;
+import xyz.ottr.lutra.system.MessageHandler;
 import xyz.ottr.lutra.system.ResultStream;
 
 public class App 
@@ -17,10 +20,13 @@ public class App
     //run this in ottr-update folder:
     //java -cp target/ottr-update-1.0-SNAPSHOT.jar:../lutra.jar update.ottr.App
     {
-        StandardTemplateManager stm = new StandardTemplateManager();
+        TemplateManager stm = new StandardTemplateManager();
         // TemplateStore tStore = stm.getStandardLibrary();
-        Format format = new StottrFormat();
-        ResultStream resultStream = stm.readInstances(format, "home/ottr/ottr-update/src/main/resources/instances");
-        resultStream.flatMap(Function.identity());
+        MessageHandler msgs = stm.readLibrary(stm.getFormat("stOTTR"), "paht/to/file.stottr");
+        msgs.printMessages();
+        ResultStream<Instance> expanded = stm.readInstances(stm.getFormat("stOTTR"), "home/ottr/ottr-update/src/main/resources/instances")
+            .innerFlatMap(stm.makeExpander());
+
+        expanded.innerForEach(i -> System.out.println(i.toString()));
     }
 }
