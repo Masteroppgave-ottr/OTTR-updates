710189203108b617794931e505a38023c334b8cc
Preben <preben.zahl@gmail.com>
2022-09-28 10:40:41 +0200

    expand instances and convert to models

--- ottr-update/src/main/java/update/ottr/App.java
+++ ottr-update/src/main/java/update/ottr/App.java
@@ -1,32 +1,56 @@
 package update.ottr;
 
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import xyz.ottr.lutra.api.StandardFormat;
 import xyz.ottr.lutra.api.StandardTemplateManager;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.StmtIterator;
+
 import xyz.ottr.lutra.TemplateManager;
 import xyz.ottr.lutra.model.Instance;
-import xyz.ottr.lutra.io.Format;
-import xyz.ottr.lutra.io.Format.Support;
-import xyz.ottr.lutra.store.TemplateStore;
-import xyz.ottr.lutra.stottr.StottrFormat;
 import xyz.ottr.lutra.system.MessageHandler;
 import xyz.ottr.lutra.system.ResultStream;
+import xyz.ottr.lutra.wottr.writer.WInstanceWriter;
 
 public class App 
 {
-    public static void main( String[] args )
-    //run this in ottr-update folder:
-    //java -cp target/ottr-update-1.0-SNAPSHOT.jar:../lutra.jar update.ottr.App
-    {
-        TemplateManager stm = new StandardTemplateManager();
-        // TemplateStore tStore = stm.getStandardLibrary();
-        MessageHandler msgs = stm.readLibrary(stm.getFormat("stOTTR"), "paht/to/file.stottr");
-        msgs.printMessages();
-        ResultStream<Instance> expanded = stm.readInstances(stm.getFormat("stOTTR"), "home/ottr/ottr-update/src/main/resources/instances")
-            .innerFlatMap(stm.makeExpander());
 
-        expanded.innerForEach(i -> System.out.println(i.toString()));
+    public static Model expandAndGetModel(String pathToInstances, TemplateManager tm)
+    {
+        // read instances from file and expand them
+        ResultStream<Instance> expanded = tm.readInstances(tm.getFormat("stOTTR"), pathToInstances).innerFlatMap(tm.makeExpander());
+        Set<Instance> instances = new HashSet<Instance>();
+        expanded.innerForEach(instances::add);
+
+        // write expanded instances to model
+        WInstanceWriter writer = new WInstanceWriter();
+        writer.addInstances(instances);
+        return writer.writeToModel();
+    }
+
+    public static void main( String[] args )
+    //to run: following command in ottr-update folder:
+    //mvn package && java -jar target/update.jar
+    {
+        String pathToOldInstances = "/home/prebz/ottr/dev/temp/old_instances.stottr";
+        String pathToNewInstances = "/home/prebz/ottr/dev/temp/new_instances.stottr";
+        String pathToTemplate = "/home/prebz/ottr/dev/temp/templates.stottr";
+
+        TemplateManager tm = new StandardTemplateManager();
+        MessageHandler msgs = tm.readLibrary(tm.getFormat("stOTTR"), pathToTemplate); 
+        msgs.printMessages();
+
+        Model oldInstances = expandAndGetModel(pathToOldInstances, tm);
+        Model newInstances = expandAndGetModel(pathToNewInstances, tm);
+        
+        // StmtIterator iterator = oldInstances.listStatements();
+        // for ( ; iterator.hasNext() ; ) {
+        //     System.out.println(iterator.nextStatement());
+        // }
+
+        // System.out.println(naiveUpdate.createQuery(oldExpanded, newExpanded));
     }
 }
