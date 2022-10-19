666cfd5850073deaaa7bc7c98e21ddcd9abd2d35
Eckhoff42 <magnus.wiik.eckhoff@gmail.com>
2022-10-19 19:09:37 +0200

    refactor. More into naive update

--- ottr-update/src/main/java/update/ottr/App.java
+++ ottr-update/src/main/java/update/ottr/App.java
@@ -5,55 +5,18 @@ import xyz.ottr.lutra.api.StandardTemplateManager;
 import xyz.ottr.lutra.TemplateManager;
 import xyz.ottr.lutra.system.MessageHandler;
 
-//jena
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.update.UpdateRequest;
-
 //java
 import java.util.ArrayList;
 import java.util.List;
-import java.io.*;
 
 public class App {
 
     public static void simpleUpdate(TemplateManager tm, Logger log, String pathToNewInstances,
             String pathToOldInstances,
             String dbURL) {
-        Diff d = new Diff(log);
-        d.readDiffFromStdIn();
-
-        log.print("DEFAULT", "Add linenumbers" + d.addLines.toString());
-        log.print("DEFAULT", "delete linenumbers" + d.deleteLines.toString());
-
-        String addInstancesString = null;
-        String deleteInstancesString = null;
-        try {
-            addInstancesString = d.getAddInstancesString(pathToNewInstances);
-            deleteInstancesString = d.getDeleteInstancesString(pathToOldInstances);
-        } catch (FileNotFoundException error) {
-            System.out.println("Could not old or new instance file");
-            error.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        log.print("DEFAULT", "String containing instances to add\n'" + addInstancesString + "'");
-        log.print("DEFAULT", "String containing instances to delete\n'" + deleteInstancesString + "'");
-
-        JenaInterface jh = new JenaInterface(log);
-
-        Model insertModel = jh.expandAndGetModelFromString(addInstancesString, tm);
-        Model deleteModel = jh.expandAndGetModelFromString(deleteInstancesString, tm);
 
         naiveUpdate nu = new naiveUpdate(log);
-        UpdateRequest updateRequest = nu.createUpdateRequest(deleteModel, insertModel);
-
-        try {
-            FusekiInterface fi = new FusekiInterface(log);
-            fi.updateLocalDB(updateRequest, dbURL);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        nu.simpleUpdate(tm, log, pathToNewInstances, pathToOldInstances, dbURL);
     }
 
     public static void main(String[] args)
@@ -68,8 +31,14 @@ public class App {
         String pathToNewInstances = "../temp/new_instances.stottr";
         String pathToTemplate = "../temp/templates.stottr";
         String dbURL = "http://localhost:3030/";
+        String[] logLevels = {
+                "DEFAULT",
+                // "FUSEKI",
+                // "JENA",
+                // "DIFF"
+        };
+        ArrayList<String> loggerLevel = new ArrayList<String>(List.of(logLevels));
 
-        ArrayList<String> loggerLevel = new ArrayList<String>(List.of("FUSEKI", "JENA", "DIFF", "DEFAULT"));
         Logger log = new Logger(loggerLevel);
         TemplateManager tm = new StandardTemplateManager();
         MessageHandler msgs = tm.readLibrary(tm.getFormat("stOTTR"), pathToTemplate);
--- ottr-update/src/main/java/update/ottr/naiveUpdate.java
+++ ottr-update/src/main/java/update/ottr/naiveUpdate.java
@@ -1,8 +1,12 @@
 package update.ottr;
 
+import java.io.FileNotFoundException;
+
 import org.apache.jena.arq.querybuilder.UpdateBuilder;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.update.UpdateRequest;
+
+import xyz.ottr.lutra.TemplateManager;
 
 public class naiveUpdate {
     private Logger log;
@@ -17,7 +21,7 @@ public class naiveUpdate {
                 .addDelete(oldModel);
 
         UpdateRequest request = builder.buildRequest();
-        log.print(logLevel, "Delete request: " + request.toString());
+        log.print(logLevel, "Delete request:\n" + request.toString());
         return request;
     }
 
@@ -26,7 +30,7 @@ public class naiveUpdate {
                 .addInsert(newModel);
 
         UpdateRequest request = builder.buildRequest();
-        log.print(logLevel, "Insert request: " + request.toString());
+        log.print(logLevel, "Insert request:\n" + request.toString());
         return request;
     }
 
@@ -42,8 +46,46 @@ public class naiveUpdate {
         }
 
         UpdateRequest request = builder.buildRequest();
-        log.print(logLevel, "Update request: " + request.toString());
+        log.print(logLevel, "Update request:+\n" + request.toString());
         return request;
     }
 
+    public void simpleUpdate(TemplateManager tm, Logger log, String pathToNewInstances,
+            String pathToOldInstances,
+            String dbURL) {
+        Diff d = new Diff(log);
+        d.readDiffFromStdIn();
+
+        log.print("DEFAULT", "Add linenumbers" + d.addLines.toString());
+        log.print("DEFAULT", "delete linenumbers" + d.deleteLines.toString());
+
+        String addInstancesString = null;
+        String deleteInstancesString = null;
+        try {
+            addInstancesString = d.getAddInstancesString(pathToNewInstances);
+            deleteInstancesString = d.getDeleteInstancesString(pathToOldInstances);
+        } catch (FileNotFoundException error) {
+            System.out.println("Could not old or new instance file");
+            error.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        log.print("DEFAULT", "String containing instances to add\n'" + addInstancesString + "'");
+        log.print("DEFAULT", "String containing instances to delete\n'" + deleteInstancesString + "'");
+
+        JenaInterface jh = new JenaInterface(log);
+        Model insertModel = jh.expandAndGetModelFromString(addInstancesString, tm);
+        Model deleteModel = jh.expandAndGetModelFromString(deleteInstancesString, tm);
+
+        naiveUpdate nu = new naiveUpdate(log);
+        UpdateRequest updateRequest = nu.createUpdateRequest(deleteModel, insertModel);
+
+        try {
+            FusekiInterface fi = new FusekiInterface(log);
+            fi.updateLocalDB(updateRequest, dbURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
