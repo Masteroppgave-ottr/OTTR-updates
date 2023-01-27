103e5b34f25780ba1f6003d5640678dea0a8638b
Eckhoff42 <magnus.wiik.eckhoff@gmail.com>
2023-01-27 06:57:27 +0100

    blank nodes are transformed into variables

--- ottr-update/src/main/java/update/ottr/BlankNode.java
+++ ottr-update/src/main/java/update/ottr/BlankNode.java
@@ -3,8 +3,12 @@ package update.ottr;
 import org.apache.jena.arq.querybuilder.SelectBuilder;
 import org.apache.jena.arq.querybuilder.UpdateBuilder;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.sparql.core.Var;
 import org.apache.jena.update.UpdateRequest;
 
 // This solution is about removing the local blank node assumption. 
@@ -21,15 +25,15 @@ public class BlankNode {
 
         int count = 0; // has to be several when considering multiple blank nodes
 
-
         // count the number of occurences of the blank node in the old model
+        log.print(LOGTAG.DEBUG, "lez go!");
 
         StmtIterator statements = deleteModel.listStatements();
         while (statements.hasNext()) {
             // if the statement contains a blank node
             Statement statement = statements.next();
             if (statement.getSubject().isAnon() || statement.getObject().isAnon()) {
-                //increment count
+                // increment count
                 count++;
             }
         }
@@ -41,23 +45,33 @@ public class BlankNode {
         while (statements.hasNext()) {
             // if the statement contains a blank node
             Statement statement = statements.next();
-            String sub = statement.getSubject().toString();
-            String obj = statement.getObject().toString();
+            log.print(LOGTAG.DEBUG, statement.toString());
+
+            String sub = null;
+            String obj = null;
             if (statement.getSubject().isAnon()) {
-                sub = sub.replaceFirst("^..", "?");
+                sub = "?" + statement.getSubject().toString();
             }
-            
+
             if (statement.getObject().isAnon()) {
-                obj = obj.replaceFirst("^..", "?");
+                obj = "?" + statement.getObject().toString();
             }
-            builder.addWhere(sub, statement.getPredicate(), obj);
+
+            if (sub != null && obj != null) {
+                builder.addWhere(sub, statement.getPredicate(), obj);
+            } else if (sub != null) {
+                builder.addWhere(sub, statement.getPredicate(), statement.getObject());
+            } else if (obj != null) {
+                builder.addWhere(statement.getSubject(), statement.getPredicate(), obj);
+            } else {
+                builder.addWhere(statement.getSubject(), statement.getPredicate(), statement.getObject());
+            }
         }
 
-        log.print(LOGTAG.DEBUG, "" + builder.build());
+        log.print(LOGTAG.DEBUG, "\n" + builder.build());
         // return request;
         return null;
     }
-
 
     // run blanknode update
 }
