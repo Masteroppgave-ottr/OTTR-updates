0eeb8c91710d499f2a789f9a3e62bd6fa9eddb41
Preben <preben.zahl@gmail.com>
2023-01-27 04:24:25 +0100

    init

---
+++ ottr-update/src/main/java/update/ottr/BlankNode.java
@@ -0,0 +1,45 @@
+package update.ottr;
+
+import org.apache.jena.arq.querybuilder.UpdateBuilder;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.update.UpdateRequest;
+
+// This solution is about removing the local blank node assumption. 
+public class BlankNode {
+    private Logger log;
+    private LOGTAG logLevel = LOGTAG.DEFAULT;
+
+    public BlankNode(Logger log) {
+        this.log = log;
+    }
+
+    public UpdateRequest createDelRequest(Model deleteModel) {
+        // find all occurences of blank nodes in the old model
+
+        int count = 0; // has to be several when considering multiple blank nodes
+
+
+        // count the number of occurences of the blank node in the old model
+
+        StmtIterator statements = deleteModel.listStatements();
+        while (statements.hasNext()) {
+            // if the statement contains a blank node
+            Statement statement = statements.next();
+            if (statement.getSubject().isAnon() || statement.getObject().isAnon()) {
+                //increment count
+                count++;
+            }
+        }
+
+        UpdateBuilder builder = new UpdateBuilder().addWhere(null)
+
+        UpdateRequest request = builder.buildRequest();
+        log.print(logLevel, "Delete request:\n" + request.toString());
+        return request;
+    }
+
+
+    // run blanknode update
+}
